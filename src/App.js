0b36012274d468809acc5947f12bb2674ccb5bbd
Trung <nguyenvantrung200819dtd@gmail.com>
2023-08-29 10:05:58 +0700

    add css && create navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Navbar from "./pages/Navbar";
+import Footer from "./pages/Footer";
 
 function App() {
   return (
     <>
+      <Navbar></Navbar>
+        <Footer></Footer>
     </>
   );
 }
