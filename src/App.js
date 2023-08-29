dacd57cf8a3502dcca3f1babd40f934cddcb5bfb
Trung <nguyenvantrung200819dtd@gmail.com>
2023-08-29 12:39:08 +0700

    ..

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Navbar from "./pages/Navbar";
 import Footer from "./pages/Footer";
+import { Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar></Navbar>
+      <Routes>
+        <Navbar></Navbar>
         <Footer></Footer>
+      </Routes>
     </>
   );
 }
