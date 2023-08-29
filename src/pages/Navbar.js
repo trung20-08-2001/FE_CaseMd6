995dd86afe3f6e674f31a51d65e692d90a789ff8
Đinh Văn Sơn <dinhvanson@MacBook-Air-cua-inh.local>
2023-08-29 15:56:51 +0700

    done login

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from "axios";
 
 
 const Navbar = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // Thêm state cho trạng thái đăng nhập
+
+
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        // Kiểm tra các trường đăng nhập
+        if (!isValidInput(username) || !isValidInput(password)) {
+            setErrorMessage('Tên đăng nhập và mật khẩu chỉ được chứa chữ cái và số.');
+            return;
+        }
+
+        const account = {
+            username: username,
+            password: password
+        }
+
+        axios.post("http://localhost:8080/api/login", account)
+            .then(data => {
+                console.log(data)
+                setErrorMessage('');
+                setIsLoggedIn(true); // Đánh dấu đã đăng nhập thành công
+
+
+
+            })
+            .catch(function (err) {
+                console.log(err)
+                setErrorMessage('Tên đăng nhập hoặc mật khẩu không chính xác.');
+
+            })
+
+    };
+    const isValidInput = (input) => {
+        const regex = /^[a-zA-Z0-9]+$/;
+        return regex.test(input);
+    };
+
     return (
         <>
             <header className="header-area">
@@ -23,37 +65,70 @@ const Navbar = () => {
                                             style={{ marginTop: "-25px" }}
                                         >
                                             <li>
-                                                {/*                                            <a><img style="border-radius: 50%;border-color: #0d0d0d" width="50"*/}
-                                                {/*                                                    height="50"*/}
-                                                {/*                                                    src="https://khoinguonsangtao.vn/wp-content/uploads/2022/02/anh-dai-dien-fb-dep.jpg"></a>*/}
-                                                {/*                                            <ul class="dropdown">*/}
-                                                {/*                                                <li><a>Thông tin cá nhân</a></li>*/}
-                                                {/*                                                <li><a>Lịch sử giao dịch</a></li>*/}
-                                                {/*                                                <li><a>Đăng xuất</a></li>*/}
-                                                {/*                                            </ul>*/}
-                                                <div className="header-login-register">
+                                                {isLoggedIn && (
+                                                    <>
+                                                        <a>
+                                                            <i
+                                                                className="fa fa-bars"></i>
+                                                            <img
+                                                                style={{ borderRadius: "50%", borderColor: "#0d0d0d" }}
+                                                                width={35}
+                                                                height={35}
+                                                                src="https://khoinguonsangtao.vn/wp-content/uploads/2022/02/anh-dai-dien-fb-dep.jpg"
+                                                            />
+                                                        </a>
+                                                        <ul className="dropdown">
+                                                            <li>
+                                                                <a>Đăng tin</a>
+                                                            </li>
+                                                            <li>
+                                                                <a>Thông tin cá nhân</a>
+                                                            </li>
+                                                            <li>
+                                                                <a>Lịch sử giao dịch</a>
+                                                            </li>
+                                                            <li>
+                                                                <a>Đăng xuất</a>
+                                                            </li>
+                                                        </ul>
+                                                    </>
+
+                                                )}
+
+
+                                                {!isLoggedIn && ( // Hiển thị phần đăng nhập chỉ khi chưa đăng nhập thành công
+
+                                                    <div className="header-login-register">
                                                     <ul className="login">
                                                         <li>
                                                             <a style={{ cursor: "pointer" }}>Login</a>
                                                             <div className="login-form">
                                                                 <h4>Login</h4>
-                                                                <form action="#" method="post">
+                                                                <form onSubmit={handleLogin}>
                                                                     <div className="input-box mb-19">
                                                                         <i className="fa fa-user" />
                                                                         <input
                                                                             type="text"
-                                                                            name="user-name"
+                                                                            value={username}
                                                                             placeholder="Username"
+                                                                            onChange={(event) => setUsername(event.target.value)}
                                                                         />
                                                                     </div>
                                                                     <div className="input-box">
                                                                         <i className="fa fa-lock" />
                                                                         <input
                                                                             type="password"
-                                                                            name="user-password"
+                                                                            value={password}
                                                                             placeholder="Password"
+                                                                            onChange={(event) => setPassword(event.target.value)}
+
                                                                         />
                                                                     </div>
+                                                                    {errorMessage && (
+                                                                        <p style={{ color: "red" }} className="error-message">
+                                                                            {errorMessage}
+                                                                        </p>
+                                                                    )}
                                                                     <div className="social-links mt-25">
                                                                         <a href="#">
                                                                             <i className="fa fa-facebook" />
@@ -123,6 +198,8 @@ const Navbar = () => {
                                                         </li>
                                                     </ul>
                                                 </div>
+                                                )}
+
                                             </li>
                                         </ul>
                                     </nav>
