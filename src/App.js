cc281165125d87b7594e47e04e2e946b8e96b5e4
Chungtien2506 <chungcoiaaaaa@gmail.com>
2023-08-30 10:32:12 +0700

    xongdktknd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./pages/Navbar";
 import Footer from "./pages/Footer";
+import Register from "./components/Register";
+
 
 function App() {
   return (
     <>
-      <Navbar></Navbar>
-        <Footer></Footer>
+      <Navbar/>
+       <Footer></Footer>
     </>
   );
 }
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Register from "../components/Register";
 
 
 const Navbar = () => {
+    // const formik = useFormik({
+    //     initialValues: {
+    //         username: '',
+    //         phone: '',
+    //         password: '',
+    //         confirmPassword: ''
+    //     },
+    //     validationSchema: Yup.object({
+    //         username: Yup.string().required("Trường này không ược để trống").matches(/^[a-zA-Z0-9]*$/, 'Phải là chữ hoặc số'),
+    //         phone: Yup.string().required('Trường này không được để trống').matches(/^[0-9]{1,10}$/, 'Phải là số và tối đa 10 chữ số'),
+    //         password: Yup.string().required('Mật khẩu không được để trống').min(6, 'Mật khẩu phải có ít nhất 6 kí tự').max(32, 'Mật khẩu không được vượt quá 32 kí tự'),
+    //         confirmPassword: Yup.string().required("Mật khẩu không được để trống").oneOf([Yup.ref("password")], "phải nhập đúng mật khẩu vừa nhập")
+    //     })
+    // })
+    // const [account, setAccount] = useState({});
+
+    // const handleInputChange = (e) => {
+    //     const {name, value} = e.target;
+    //     setAccount({...account, [name]: value});
+    // };
+
+    // const create = () => {
+    //     axios.post("http://localhost:8080/accounts/createAccount", account)
+    //         .then(data => {
+    //             alert("đăng kí thành công")
+    //         })
+    //         .catch(error => console.log(error))
+    // }
     return (
         <>
             <header className="header-area">
@@ -11,7 +40,7 @@ const Navbar = () => {
                             <div className="col-lg-2">
                                 <div className="logo">
                                     <a href="index.html">
-                                        <img src="images/logo/logo.png" alt="DomInno" />
+                                        <img src="images/logo/logo.png" alt="DomInno"/>
                                     </a>
                                 </div>
                             </div>
@@ -20,7 +49,7 @@ const Navbar = () => {
                                     <nav id="primary-menu">
                                         <ul
                                             className="main-menu text-right"
-                                            style={{ marginTop: "-25px" }}
+                                            style={{marginTop: "-25px"}}
                                         >
                                             <li>
                                                 {/*                                            <a><img style="border-radius: 50%;border-color: #0d0d0d" width="50"*/}
@@ -34,12 +63,12 @@ const Navbar = () => {
                                                 <div className="header-login-register">
                                                     <ul className="login">
                                                         <li>
-                                                            <a style={{ cursor: "pointer" }}>Login</a>
+                                                            <a style={{cursor: "pointer"}}>Login</a>
                                                             <div className="login-form">
                                                                 <h4>Login</h4>
                                                                 <form action="#" method="post">
                                                                     <div className="input-box mb-19">
-                                                                        <i className="fa fa-user" />
+                                                                        <i className="fa fa-user"/>
                                                                         <input
                                                                             type="text"
                                                                             name="user-name"
@@ -47,7 +76,7 @@ const Navbar = () => {
                                                                         />
                                                                     </div>
                                                                     <div className="input-box">
-                                                                        <i className="fa fa-lock" />
+                                                                        <i className="fa fa-lock"/>
                                                                         <input
                                                                             type="password"
                                                                             name="user-password"
@@ -56,10 +85,10 @@ const Navbar = () => {
                                                                     </div>
                                                                     <div className="social-links mt-25">
                                                                         <a href="#">
-                                                                            <i className="fa fa-facebook" />
+                                                                            <i className="fa fa-facebook"/>
                                                                         </a>
                                                                         <a href="#">
-                                                                            <i className="fa fa-google-plus" />
+                                                                            <i className="fa fa-google-plus"/>
                                                                         </a>
                                                                         <button
                                                                             type="submit"
@@ -74,51 +103,10 @@ const Navbar = () => {
                                                     </ul>
                                                     <ul className="login">
                                                         <li>
-                                                            <a style={{ cursor: "pointer" }}>Register</a>
+                                                            <a style={{cursor: "pointer"}}>Register</a>
                                                             <div className="login-form">
                                                                 <h4>Sign Up</h4>
-                                                                <form>
-                                                                    <div className="input-box mb-19">
-                                                                        <i className="fa fa-user" />
-                                                                        <input
-                                                                            type="text"
-                                                                            name="username"
-                                                                            placeholder="Username"
-                                                                        />
-                                                                    </div>
-                                                                    <div className="input-box mb-19">
-                                                                        <i className="fa fa-phone" />
-                                                                        <input
-                                                                            type="password"
-                                                                            name="phone"
-                                                                            placeholder="Phone"
-                                                                        />
-                                                                    </div>
-                                                                    <div className="input-box mb-19">
-                                                                        <i className="fa fa-lock" />
-                                                                        <input
-                                                                            type="text"
-                                                                            name="password"
-                                                                            placeholder="Password"
-                                                                        />
-                                                                    </div>
-                                                                    <div className="input-box mb-19">
-                                                                        <i className="fa fa-lock" />
-                                                                        <input
-                                                                            type="text"
-                                                                            name="confirmPassword"
-                                                                            placeholder="ConfirmPassword"
-                                                                        />
-                                                                    </div>
-                                                                    <div className="button-box mt-20">
-                                                                        <button
-                                                                            type="submit"
-                                                                            className="register-btn button lemon pull_right"
-                                                                        >
-                                                                            Sign Up
-                                                                        </button>
-                                                                    </div>
-                                                                </form>
+                                                               <Register/>
                                                             </div>
                                                         </li>
                                                     </ul>
