7c34900139b32679c94958b18e78a2ca8cbc2f20
Trung <nguyenvantrung200819dtd@gmail.com>
2023-08-30 08:05:12 +0700

    create_house_done

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,3 +1,10 @@
-import axiosInstance from "./api"
+import { createAsyncThunk } from "@reduxjs/toolkit"
+import customAxios from "./api"
 
-export const findAllCategory=()=> axiosInstance.get("/categories/findAll")
+export const getAllCategory = createAsyncThunk(
+    "categorise/getAllCategory",
+    async() => {
+        const response = await customAxios.get("/categories/findAll")
+        return response.data;
+    }
+)
