a06a3fcadf2baf525c39ca7055bdc9ef33527b8a
Chungtien2506 <chungcoiaaaaa@gmail.com>
2023-09-05 08:52:30 +0700

    xong

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-
-import { Routes,Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Master from "./components/layout/Master";
 import Host from "./components/Host";
 import CreateHouse from "./pages/CreateHouse";
+import MyHouses from "./pages/MyHouses";
+import EditHouse from "./pages/EditHouse";
+import Home from "./pages/Home";
+import UpRole from "./pages/UpRole";
+import ChangePassword from "./pages/ChangePassword";
+
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="" element={<Master/>}>
-          <Route path="host" element={<Host/>}>
-            <Route  index element={<CreateHouse/>}></Route>
-          </Route>
-        </Route>
-      </Routes>
-    </>
-  );
+    let account = JSON.parse(localStorage.getItem("account"));
+
+    return (
+        <>
+            <Routes>
+                <Route path="" element={<Master />}>
+                    <Route index element={<Home />}></Route>
+                    {account !== null && account.role.id === 2 &&
+                        <Route path="host" element={<Host />}>
+                            <Route path="create_house" element={<CreateHouse />}></Route>
+                            <Route index element={<MyHouses />}></Route>
+                            <Route path="edit_house/:indexHouseEdit" element={<EditHouse />}></Route>
+                        </Route>
+                    }
+                    <Route path={"user"} element={<UpRole/>}></Route>
+                    <Route path={"changePassword"} element={<ChangePassword></ChangePassword>}></Route>
+                </Route>
+
+            </Routes>
+        </>
+    );
 }
 
 export default App;
