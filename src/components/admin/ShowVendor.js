787670775f9f5d711af0fb093c4feef4e904a36a
dinhvanson <dinhsonmta55@gmail.com>
2023-09-12 08:25:20 +0700

    fix upRole

--- src/components/admin/ShowVendor.js
+++ src/components/admin/ShowVendor.js
@@ -42,7 +42,7 @@ function ShowVendor() {
                     <td>
                         {vendor.account.status.id === 2 ? (
                             <div>
-                                <Link className="btn btn-outline-secondary" to={`/myaccounout/vendor/upRole/${vendor.account.id}`}>
+                                <Link className="btn btn-outline-secondary" to={`/myaccount/vendor/upRole/${vendor.account.id}`}>
                                     Up Role
                                 </Link>
                             </div>
