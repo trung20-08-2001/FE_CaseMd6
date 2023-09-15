ef549156668c5c98b3f9580ce1d8c86f6a6922e2
Chungtien2506 <chungcoiaaaaa@gmail.com>
2023-09-15 11:26:30 +0700

    ok

--- src/components/TopHouse.js
+++ src/components/TopHouse.js
@@ -3,17 +3,25 @@ import { useDispatch, useSelector } from 'react-redux'
 import { findTopHouse } from '../services/houseService';
 import Loading from './Loading';
 
-
-
 function TopHouse() {
     const dispatch = useDispatch();
     const topHouse = useSelector(state => state.house.topHouse);
-   
+
     useEffect(() => {
         if (topHouse.length === 0) {
             dispatch(findTopHouse())
         }
     }, [])
+
+    // CSS cho hiệu ứng hover
+    const hoverEffectStyle = {
+        transition: 'transform 0.3s ease, box-shadow 0.3s ease',
+    };
+
+    const hoverStyle = {
+        transform: 'scale(1.05)',
+        boxShadow: '0 10px 20px rgba(0, 0, 0, 0.2)',
+    };
 
     return (
         <>
@@ -22,28 +30,23 @@ function TopHouse() {
                     <div className="row">
                         <div className="col-lg-12">
                             <div className="section-title mb-38 mt-31 text-center">
-                                {/* <span className="opacity-text text-uppercase center">
-                        TOP 5 HOUSE
-                    </span> */}
                                 <h2 className="uppercase mb-25">TOP HOUSE</h2>
                             </div>
                         </div>
                     </div>
                     <div className="row">
-
-
                         {topHouse.length !==0 ?
                             topHouse.map(item => {
                                 return (
-                                    < div className="col-lg-4 mb-20" key={item.house.id}>
-                                        <div className="single-property hover-effect-two">
+                                    <div className="col-lg-4 mb-20" key={item.house.id} style={hoverEffectStyle}>
+                                        <div className="single-property hover-effect-two" style={hoverStyle}>
                                             <div className="property-title fix pl-18 pr-18 pt-22 pb-18 bg-violet">
                                                 <div className="title-left pull_left">
                                                     <h4 className="text-white mb-12">
                                                         <a href="properties-details.html">{item.house.name}</a>
                                                     </h4>
                                                     <span>
-                                                        <  span className="mr-10">
+                                                        <span className="mr-10">
                                                             <img src="images/icons/map.png" alt="" />
                                                         </span>
                                                         {item.house.address}
@@ -54,8 +57,6 @@ function TopHouse() {
                                                 </div>
                                             </div>
                                             <div className="property-image">
-                                                {/* <Link to={"houseDetail/"+item.house.id}></Link>      */}
-
                                                 <img src={item.images.length!==0?item.images[0].url:""} alt="" style={{width:"100%",height:"300px"}} />
                                                 <div className="hover-container pl-15 pr-15 pt-16 pb-15">
                                                     <div className="hover-item">
@@ -71,13 +72,12 @@ function TopHouse() {
                                                     </div>
                                                     <div className="hover-item">
                                                         <i className='fa fa-heart mr-10' style={{ color: "red" }}></i>
-
                                                         <span>{item.house.numberOfHire}</span>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div >
+                                    </div>
                                 )
                             })
                             : <Loading></Loading>
@@ -89,4 +89,4 @@ function TopHouse() {
     )
 }
 
-export default TopHouse
+export default TopHouse;
